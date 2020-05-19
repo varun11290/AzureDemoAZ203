510fa01d6f96b2e63557d376fad114f27cc4ecd9
Varun Jain <varunjain11290@gmail.com>
2020-05-19 23:59:15 +0530

    final changes to programm.cs

--- Storage Account/blobdemo/Program.cs
+++ Storage Account/blobdemo/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using Microsoft.Azure.Storage.Blob;
-using Microsoft.Azure.Storage;
 
 namespace blobdemo
 {
@@ -8,53 +6,17 @@ namespace blobdemo
     {
         static void Main(string[] args)
         {
-            //Geting the values from config files
-            
-            var blobName="my-photo.json";
-            var filName="./Config.json";
 
             try
             {
-                // CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
-                // CloudBlobClient blobClient = account.CreateCloudBlobClient();
-
-                // //Create(blobClient, containerName);
-                // UploadFile(blobClient,containerName,blobName,filName);
+                Console.WriteLine("Start the demo");
+                BlobOperation.BlobOperations();
+                Console.WriteLine("Finish the demo");
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
                 throw ex;
-            }
-
-            Console.ReadLine();
-        }
-
-        static async void Create(CloudBlobClient blobClient, string name)
-        {
-
-            try
-            {
-                CloudBlobContainer container = blobClient.GetContainerReference(name);
-                await container.CreateAsync();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        static async void UploadFile(CloudBlobClient blobClient,string containerName,string blobName,string fileName){
-            try
-            {
-                CloudBlobContainer container=blobClient.GetContainerReference(containerName);
-                
-                var blob=container.GetBlockBlobReference(blobName);
-                await blob.UploadFromFileAsync(fileName);
-
-            }
-            catch (System.Exception)
-            {
-                throw;
             }
         }
 
