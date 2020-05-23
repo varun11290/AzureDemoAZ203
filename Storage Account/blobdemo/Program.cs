a43afb6f450ae41c1d48a719258e761ea8e9fcec
Varun Jain <varunjain11290@gmail.com>
2020-05-23 08:23:57 +0530

    Cleaning the repo

---
+++ Storage Account/blobdemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace blobdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Please add your azure storage connection string to config.json
+            try
+            {
+                Console.WriteLine("Start the demo");
+                BlobOperation.BlobOperations();
+                Console.WriteLine("Finish the demo");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ Storage Account/java-blob-demo/src/main/java/blobdemojava/Config.java
@@ -0,0 +1,20 @@
+package blobdemojava;
+
+import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Config {
+
+    // public static String GetPropertyValue(String property) {
+    //     // Properties prop = new Properties();
+
+    //     // InputStream propertyStream = BlobBasics.class.getClassLoader().getResourceAsStream("config.properties");
+    //     // if (propertyStream != null) {
+    //     //     prop.load(propertyStream);
+    //     // } else {
+    //     //     throw new RuntimeException();
+    //     // }
+
+    // }
+}
